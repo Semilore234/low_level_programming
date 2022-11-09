3b1d0744b8f3f15b1bf684334002bdfbc93d4b78
George <semiloreoluwaseyi20@gmail.com>
2022-11-09 13:38:01 +0100

    pun

---
+++ c-fundamentals/pun.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints To C, or not to C: that is the question.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("To C, or not to C: that is the question.\n");
+
+	return (0);
+}
